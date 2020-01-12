0bf31c441d3b15bbe399cffd57d1d7b1ab50eac2
Chandan Kumar <cksharma122@gmail.com>
2020-01-12 12:58:08 +0530

    Initial spike for GOL

--- src/gol/core.clj
+++ src/gol/core.clj
@@ -10,16 +10,30 @@
 ;Any dead cell with three live neighbors becomes a live cell.
 ;All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
-(def live-cells #{[0 1] [1 1] [1 0]})
+(def live-cells #{[2 1] [2 2] [2 3]})
 (def neighbour-deltas (for [x [-1 0 1] y [-1 0 1] :when (not= x y 0)] [x y]))
 (defn find-neighbours [cell] (mapv #(mapv + cell %1) neighbour-deltas))
 
-(apply conj live-cells
-       (map first
-            (filter #(> (second %) 2)
-                    (frequencies (mapcat find-neighbours live-cells)))))
+(defn neighbours-count [live-cells]
+  (frequencies (mapcat find-neighbours live-cells)))
 
-(apply disj live-cells
-       (map first
-            (filter #(> (second %) 3)
-                    (frequencies (mapcat find-neighbours live-cells)))))
+(defn determine-alive-cells [live-cells]
+  (map first (filter #(> (second %) 2) (neighbours-count live-cells))))
+
+(defn determine-dead-cells [live-cells]
+  (map first (filter #(or (> (second %) 3) (< (second %) 2)) (neighbours-count live-cells))))
+
+(defn next-generation [live-cells]
+  (apply conj
+         (apply disj live-cells
+                (determine-dead-cells live-cells))
+         (determine-alive-cells live-cells)))
+
+;(next-generation live-cells)
+(take 5 (iterate next-generation live-cells))
+
+(#{[2 2] [2 3] [2 1]}
+ #{[2 2] [1 2] [3 2]}
+ #{[2 2] [2 3] [2 1]}
+ #{[2 2] [1 2] [3 2]}
+ #{[2 2] [2 3] [2 1]})
