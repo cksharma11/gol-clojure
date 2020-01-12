9de55e3121302076e4c7b1de63f1cb153e9b2600
Chandan Kumar <cksharma122@gmail.com>
2020-01-12 15:54:44 +0530

    Refactor : Use some-fn

--- src/gol/core.clj
+++ src/gol/core.clj
@@ -1,27 +1,26 @@
 (ns gol.core)
 
-;Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-;Any live cell with two or three live neighbours lives on to the next generation.
-;Any live cell with more than three live neighbours dies, as if by overpopulation.
-;Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-;These rules, which compare the behavior of the automaton to real~~ life, can be condensed into the following:
-;
-;Any live cell with two or three neighbors survives.
-;Any dead cell with three live neighbors becomes a live cell.
-;All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-
 (def live-cells #{[2 1] [2 2] [2 3]})
 (def neighbour-deltas (for [x [-1 0 1] y [-1 0 1] :when (not= x y 0)] [x y]))
-(defn find-neighbours [cell] (mapv #(mapv + cell %1) neighbour-deltas))
 
-(defn neighbours-count [live-cells]
+(defn find-neighbours [cell] (mapv #(mapv + cell %1) neighbour-deltas))
+(defn neighbours-frequency [live-cells]
   (frequencies (mapcat find-neighbours live-cells)))
 
+(defn exact-three-neighbours [neighbours-count] (= neighbours-count 3))
+(defn more-than-three-neighbours [neighbours-count] (> neighbours-count 3))
+(defn less-than-two-neighbours [neighbours-count] (< neighbours-count 2))
+
+(defn any-rule-matching? [rules [_ freq]] ((apply some-fn rules) freq))
+
+(defn filter-by-rules [rules live-cells]
+  (map first (filter (partial any-rule-matching? rules) (neighbours-frequency live-cells))))
+
 (defn determine-alive-cells [live-cells]
-  (map first (filter #(> (second %) 2) (neighbours-count live-cells))))
+  (filter-by-rules [exact-three-neighbours] live-cells))
 
 (defn determine-dead-cells [live-cells]
-  (map first (filter #(or (> (second %) 3) (< (second %) 2)) (neighbours-count live-cells))))
+  (filter-by-rules [more-than-three-neighbours less-than-two-neighbours] live-cells))
 
 (defn next-generation [live-cells]
   (apply conj
@@ -29,11 +28,7 @@
                 (determine-dead-cells live-cells))
          (determine-alive-cells live-cells)))
 
-;(next-generation live-cells)
-(take 5 (iterate next-generation live-cells))
+(defn nth-generation [current-generation gen-number]
+  (nth (iterate next-generation current-generation) (inc gen-number)))
 
-(#{[2 2] [2 3] [2 1]}
- #{[2 2] [1 2] [3 2]}
- #{[2 2] [2 3] [2 1]}
- #{[2 2] [1 2] [3 2]}
- #{[2 2] [2 3] [2 1]})
+(nth-generation live-cells 5)
